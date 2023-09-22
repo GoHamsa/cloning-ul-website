dd19dabeada11e74fc87ee5bd6430d7658596192
Hamsa <hamsa.g@outlook.com>
2023-09-22 13:54:42 +0200

    Add version for retro

--- src/App.js
+++ src/App.js
@@ -1,30 +1,232 @@
+//import pics from 'C:/Users/Hamsa/projects/cloning-ul-website/pics';
+import React from 'react';
 import styles from './App.module.scss';
 
 export default function App() {
   return (
     <>
       <main>
-        <section>Section 1</section>
-        <header>
+        <header className={styles.container}>
           <nav>
-            <a href="/#">Logo</a>
+            <a href="https://eclipsefin.netlify.app/" className={styles.logo}>
+              Eclipse
+            </a>
             <ul>
               <li>
-                <a href="/#">Features</a>
+                <a href="https://eclipsefin.netlify.app/#features">Features</a>
               </li>
               <li>
-                <a href="/#">Pricing</a>
+                <a href="https://eclipsefin.netlify.app/#pricing">Pricing</a>
               </li>
               <li>
-                <a href="/#">FAQs</a>
+                <a href="https://eclipsefin.netlify.app/#faqs">FAQs</a>
               </li>
             </ul>
-            <a href="/#">Start Now</a>
+            <a
+              href="https://eclipsefin.netlify.app/#pricing"
+              className={styles.baseButton}
+            >
+              Start Now >
+            </a>
           </nav>
         </header>
-        <section>Section 2</section>
-        <section>Section 3</section>
+        <section>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <h1>
+            All-in-one Finance App
+            <br />
+            you always wanted
+          </h1>
+          <br />
+          <p>
+            Gain control of your finances with our all-in-one finance app. Track
+            your
+            <br />
+            spending, find investment opportunities, and learn.
+          </p>
+          <br />
+          <a
+            href="https://eclipsefin.netlify.app/#pricing"
+            className={styles.getStarted}
+          >
+            Get Started >
+          </a>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </section>
+        <section>
+          <img src="/pic1.png" />
+        </section>
+        <br />
+        <br />
+        <section>
+          <p className={styles.myHeadline}>Start growing your wealth with</p>
+          <p className={styles.myHeadline}>Eclipse</p>
+          <br />
+          <div>
+            All-in-one personal finance app that simplifies investment
+            opportunities, expense
+            <br />
+            tracking, and financial news for users..
+          </div>
+        </section>
+        <section>
+          <div className={styles.gridContainer}>
+            <div className="gridItem">
+              <img
+                src="blitz.png"
+                alt="Description"
+                className={styles.gridImage}
+              />
+              <h4>Automated Expense Tracking</h4>
+              <p>
+                Eclipse automatically tracks your expenses and provides a
+                monthly report, in just a single click.
+              </p>
+            </div>
+            <div className="gridItem">
+              <img
+                src="blitz.png"
+                alt="Description"
+                className={styles.gridImage}
+              />
+              <h4>Automated Expense Tracking</h4>
+              <p>
+                Eclipse automatically tracks your expenses and provides a
+                monthly report, in just a single click.
+              </p>
+            </div>
+            <div className="gridItem">
+              <img
+                src="blitz.png"
+                alt="Description"
+                className={styles.gridImage}
+              />
+              <h4>Automated Expense Tracking</h4>
+              <p>
+                Eclipse automatically tracks your expenses and provides a
+                monthly report, in just a single click.
+              </p>
+            </div>
+            <div className="gridItem">
+              <img
+                src="blitz.png"
+                alt="Description"
+                className={styles.gridImage}
+              />
+              <h4>Automated Expense Tracking</h4>
+              <p>
+                Eclipse automatically tracks your expenses and provides a
+                monthly report, in just a single click.
+              </p>
+            </div>
+            <div className="gridItem">
+              <img
+                src="blitz.png"
+                alt="Description"
+                className={styles.gridImage}
+              />
+              <h4>Automated Expense Tracking</h4>
+              <p>
+                Eclipse automatically tracks your expenses and provides a
+                monthly report, in just a single click.
+              </p>
+            </div>
+            <div className="gridItem">
+              <img
+                src="blitz.png"
+                alt="Description"
+                className={styles.gridImage}
+              />
+              <h4>Automated Expense Tracking</h4>
+              <p>
+                Eclipse automatically tracks your expenses and provides a
+                monthly report, in just a single click.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <p className={styles.myHeadline}>
+            Build a head start to your finances
+          </p>
+          <br />
+          <p>
+            Gain control of your finances with our all-in-one finance app. Track
+            your
+            <br />
+            spending, find investment opportunities, and learn.
+          </p>
+          <br />
+          <a
+            href="https://eclipsefin.netlify.app/#pricing"
+            className={styles.getStarted}
+          >
+            Start Now >
+          </a>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </section>
+        <section>Section 6</section>
+        <section>Section 7</section>
+        <section>Section 8</section>
       </main>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <footer>Footer</footer>
     </>
   );
